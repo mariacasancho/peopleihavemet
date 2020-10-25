ee99eff803b3dc2578922a20d9d1ebf558ed9ece
mariacasancho <mariacasancho@gmail.com>
2020-10-25 12:00:14 +0100

    tablesearch

--- components/MeetingTable.js
+++ components/MeetingTable.js
@@ -1,29 +1,102 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from "react";
+import { Table, Input, Button, Space } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import Highlighter from "react-highlight-words";
 
 const MeetingTable = ({ meetings }) => {
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({
+            setSelectedKeys,
+            selectedKeys,
+            confirm,
+            clearFilters,
+        }) => (
+                <div style={{ padding: 8 }}>
+                    <Input
+                        placeholder={`Search ${dataIndex}`}
+                        value={selectedKeys[0]}
+                        onChange={(e) =>
+                            setSelectedKeys(e.target.value ? [e.target.value] : [])
+                        }
+                        onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        style={{ width: 188, marginBottom: 8, display: "block" }}
+                    />
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                            icon={<SearchOutlined />}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Search
+                        </Button>
+                        <Button
+                            onClick={() => handleReset(clearFilters)}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Reset
+                        </Button>
+                    </Space>
+                </div>
+            ),
+        filterIcon: (filtered) => (
+            <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                ? record[dataIndex]
+                    .toString()
+                    .toLowerCase()
+                    .includes(value.toLowerCase())
+                : "",
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ""}
+                />
+            ) : (
+                    text
+                ),
+    });
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText("");
+    };
+
     const columns = [
         {
-            title: 'Person',
-            dataIndex: 'person',
+            title: "Person",
+            dataIndex: "person",
+            ...getColumnSearchProps("person"),
         },
         {
-            title: 'Date',
-            dataIndex: 'date',
+            title: "Date",
+            dataIndex: "date",
+            ...getColumnSearchProps("date"),
         },
         {
-            title: 'Location',
-            dataIndex: 'location_name',
+            title: "Location",
+            dataIndex: "location_name",
+            ...getColumnSearchProps("location_name"),
         },
     ];
 
-    const onChange = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
-    };
-
-    return (
-        <Table columns={columns} dataSource={meetings} onChange={onChange} />
-    );
+    return <Table columns={columns} dataSource={meetings} />;
 };
 
 export default MeetingTable;
